f8fffd489e19bf829e12dd564acb6b9565395b3f
James McNeil <mcneil_james@hotmail.com>
2022-04-21 22:29:03 +0100

    working on formatGuess in useWordle hook

---
+++ src/components/Wordle.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+import useWordle from '../hooks/useWordle'
+
+const Wordle = ({ solution }) => {
+  const { handleKeyUp, currentGuess } = useWordle(solution)
+
+  useEffect(() => {
+    window.addEventListener('keyup', handleKeyUp)
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [handleKeyUp])
+  return <div>Current guess: {currentGuess}</div>
+}
+
+export default Wordle
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,12 +4,14 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0)
   const [currentGuess, setCurrentGuess] = useState('')
   const [guesses, setGuesses] = useState([]) // each guess is an array
-  const [history, setHistory] = useState([]) // each guess is a string
+  const [history, setHistory] = useState(['hello']) // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false)
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
-  const formatGuess = () => {}
+  const formatGuess = () => {
+    console.log(`formatting the guess ${currentGuess}`)
+  }
 
   // add e new guess to the guesses state
   // update the isCorrect state if the guess is correct
@@ -18,7 +20,41 @@ const useWordle = (solution) => {
 
   // handle keyup event & track current guess
   // if the user presses enter, add the new guess
-  const handleKeyUp = () => {}
+  const handleKeyUp = ({ key }) => {
+    if (key === 'Enter') {
+      // only add guess if turn is less then 5
+      if (turn > 5) {
+        console.log('use all your guesses')
+        return
+      }
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log('you have already tried this word')
+        return
+      }
+      //  check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log('this word must be 5 chars long')
+        return
+      }
+      formatGuess()
+    }
+    if (key === 'Backspace') {
+      setCurrentGuess((prevState) => {
+        return prevState.slice(0, -1)
+      })
+      return
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prevState) => {
+          return prevState + key
+        })
+        console.log(key)
+      }
+    }
+  }
 
   return { turn, currentGuess, guesses, isCorrect, handleKeyUp }
 }
