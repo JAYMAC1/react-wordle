fec36492fccf21c48a563635f7faeefb559dee05
James McNeil <mcneil_james@hotmail.com>
2022-04-24 10:30:48 +0100

    completeing the row and grid gameboard

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Row = ({ guess }) => {
+const Row = ({ guess, currentGuess }) => {
   if (guess) {
     return (
       <div className='row past'>
@@ -13,6 +13,21 @@ const Row = ({ guess }) => {
     )
   }
 
+  if (currentGuess) {
+    let letters = currentGuess.split('')
+    return (
+      <div className='row current'>
+        {letters.map((letter, i) => (
+          <div key={i} className='filled'>
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((v, i) => (
+          <div key={i}></div>
+        ))}
+      </div>
+    )
+  }
   return (
     <div className='row'>
       <div></div>
